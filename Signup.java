ca720cf97b9b538fd744477c2cba4d042edad7ac
ConnectedHeart <67678521+ConnectedHeart@users.noreply.github.com>
2022-08-08 18:35:05 +0900

    Add files via upload

---
+++ Signup.java
@@ -0,0 +1,22 @@
+package com.jsp.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/signup")
+public class Signup extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String url = "/WEB-INF/view/SignupMain.jsp";
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
